313421e0d2074ef19515509fdbdcab4d304a948e
juanjoseArangom <jarangoma@unal.edu.co>
2024-03-04 11:27:27 -0500

    cambios

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -7,6 +7,7 @@ public class Grupo {
     public Asignatura asignatura;
     public int codigo = 0;
     public String horario;
+    
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         this.estudiantes = estudiantes;
@@ -14,12 +15,11 @@ public class Grupo {
         this.asignatura = asignatura;
         this.codigo = codigo;
         this.horario = horario;
-        Persona[] personas = new Persona[estudiantes.length];
     }
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this(personas, profesor, asignatura, codigo, horario);
-        Persona[] personas = new Persona[cantidadEstudiantes];
+        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
+
 
     }
 
